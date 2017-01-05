09002cabfc9df5b7b12a9cb396f17e4968873c02
Rémy Kaloustian <remy.kaloustian@gmail.com>
2017-01-05 15:39:28 +0100

    First working page

--- client-web/app/scripts/app.js
+++ client-web/app/scripts/app.js
@@ -1,6 +1,2 @@
-'use strict';
-
-var modules = [require('angular-ui-router')];
-// Si on n'utilise pas Browserify
-// var modules = ['ui-router'];
-var app = angular.module('myApp', modules);
+//main variable
+var app = angular.module('eldoragoApp',[]);
