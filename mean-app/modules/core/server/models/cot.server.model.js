5da3dc2e53016af12d12888b3a89abaaae4b33c2
Flavian JACQUOT <flavian.jacquot@gmail.com>
2017-01-06 03:03:23 +0100

    suite de l'exemple

---
+++ mean-app/modules/core/server/models/cot.server.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Cot Schema
+ */
+var CotSchema = new Schema({
+  // Cot model fields
+  // ...
+});
+
+mongoose.model('Cot', CotSchema);
