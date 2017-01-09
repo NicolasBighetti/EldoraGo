e5039cc60577932cc555c86cb4c3c1b5fad3b4fe
Alicia Marin <ma202240@etu.unice.fr>
2017-01-09 16:20:37 +0100

    modification des détail d'un seul POI TGJ-48

--- client-web-2/app/scripts/controllers/poi-list.js
+++ client-web-2/app/scripts/controllers/poi-list.js
@@ -1,6 +1,18 @@
 angular.module('eldoragoApp')
   .controller('PoiListCtrl', function($scope) {
 
+    $scope.isEdited = false;
+    $scope.btnMsg = "edit";
+
+    $scope.edit = function () {
+      $scope.isEdited = true;
+    }
+
+    $scope.save = function () {
+      $scope.isEdited = false;
+      // Save en BDD
+    }
+
     $scope.map = {
         center: {
             latitude: 50.6278,
@@ -23,4 +35,11 @@ angular.module('eldoragoApp')
             options: {} // define when map is ready
         }
     };
+
+    $scope.listPois = [{
+      name: "Monument : La sculpture de Louis XIV",
+      image: "../images/louis_xiv.jpg",
+      desc: "Cette statue équestre de Louis XIV est une statue équestre en bronze de Louis XIV située sur la place d'Armes devant le château de Versailles. Jusqu'en 2008-2009, elle se trouvait dans la cour d'Honneur."
+    }];
+
     });
