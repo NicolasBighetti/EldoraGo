064293de8b2dac518ec296e3f2be5ca8994a3d75
Alicia Marin <ma202240@etu.unice.fr>
2017-01-10 17:28:07 +0100

    TGJ-5 corrige des bugs d'affichage

--- client-web-2/app/scripts/controllers/cot-list.js
+++ client-web-2/app/scripts/controllers/cot-list.js
@@ -1,16 +1,24 @@
 angular.module('eldoragoApp')
   .controller('CotListCtrl', function($scope, $http) {
 
-    // $scope.listCots = [{
-    //   name: "Chasse aux trésors du port"
-    //   },
-    //   {
-    //     name: "Le vieux Chateau"
-    //   }
-    // ];
+    $scope.init = function() {
+      $scope.getCotList();
+    }
 
-    $http.get("https://eldorago.herokuapp.com/api/cots").then(function(resp) {
-      $scope.listCots = resp.data;
-    });
+    $scope.getCotList = function() {
+      $http.get("https://eldorago.herokuapp.com/api/cots").then(function(resp) {
+        $scope.listCots = resp.data;
+      });
+    }
+
+    $scope.removeCot = function(id) {
+      $http.delete("https://eldorago.herokuapp.com/api/cots/" + id).then(function(resp) {
+        console.log("cot " + id + "delete");
+        $scope.getCotList();
+      }, function(error) {
+        alert(error);
+      });
+    }
+
 
   });
