87d9a074ef364bed56651f379b47c8b4e316a106
Alicia Marin <ma202240@etu.unice.fr>
2017-01-10 21:54:30 +0100

    TGJ-5 prêt pour la démo

--- client-web-2/app/scripts/controllers/cot-list.js
+++ client-web-2/app/scripts/controllers/cot-list.js
@@ -8,7 +8,7 @@ angular.module('eldoragoApp')
 
     $scope.getCotList = function() {
       $http.get("https://eldorago.herokuapp.com/api/cots").then(function(resp) {
-        $scope.listCots = resp.data;
+        $scope.cotList = resp.data;
       });
     }
 
@@ -20,8 +20,28 @@ angular.module('eldoragoApp')
     $scope.removeCot = function(id) {
       $http.delete("https://eldorago.herokuapp.com/api/cots/" + id).then(function(resp) {
         console.log("cot " + id + " delete");
+        var removedCot = resp.data;
 
-        // delete en cascade ??
+        $scope.getCotList();
+
+        // delete en cascade -- steps
+        for (var i; i < removedCot.steps.length; i++) {
+          $http.delete("https://eldorago.herokuapp.com/api/steps/" + removedCot.steps[i]).then(function(resp) {
+            console.log("step " + id + " delete");
+            var removedStep = resp.data;
+
+            // delete en cascade -- quests
+            for (var j; j < removedStep.quests.length; j++) {
+              $http.delete("https://eldorago.herokuapp.com/api/quests/" + id).then(function(resp) {
+                console.log("quest " + id + " delete");
+              }, function(error) {
+                alert(error);
+              });
+            }
+          }, function(error) {
+            alert(error);
+          });
+        }
 
         $scope.getCotList();
       }, function(error) {
@@ -29,5 +49,39 @@ angular.module('eldoragoApp')
       });
     }
 
+    $scope.removeStep = function(id) {
+      $http.delete("https://eldorago.herokuapp.com/api/steps/" + id).then(function(resp) {
+        console.log("cot " + id + " delete");
+        $scope.getAllStepList();
+      }, function(error) {
+        alert(error);
+      });
+    }
 
+    $scope.removeQuest = function(id) {
+      $http.delete("https://eldorago.herokuapp.com/api/quests/" + id).then(function(resp) {
+        console.log("quest " + id + " delete");
+
+        $scope.getAllQuestList();
+      }, function(error) {
+        alert(error);
+      });
+    }
+
+
+    // $scope.getAllStepList = function() {
+    //     $http.get("https://eldorago.herokuapp.com/api/steps/").then(function(resp) {
+    //       $scope.stepList = resp.data;
+    //     }, function(error) {
+    //       alert(error);
+    //     });
+    // }
+
+    // $scope.getAllQuestList = function() {
+    //     $http.get("https://eldorago.herokuapp.com/api/quests/").then(function(resp) {
+    //       $scope.questList = resp.data;
+    //     }, function(error) {
+    //       alert(error);
+    //     });
+    // }
   });
