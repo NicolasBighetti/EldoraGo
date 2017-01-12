118a4d719070e4385c4c08a698670bf1488e712f
Nicolas Bighetti <nicobighetti@gmail.com>
2017-01-12 13:00:49 +0100

    choix de couleurs pour les marqueurs Gmap
    TGJ-119

--- client-mobile/www/js/controllers.js
+++ client-mobile/www/js/controllers.js
@@ -213,7 +213,8 @@ $scope.remove = function(chat) {
       var marker = new google.maps.Marker({
         position: latLng,
         map: map,
-        title: 'Hello World!'
+        title: 'Hello World!',
+        icon: pinSymbol("#FFF")
       });
 
 
@@ -254,6 +255,16 @@ $scope.remove = function(chat) {
    })
   }
   
+  function pinSymbol(color) {
+    return {
+        path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z M -2,-30 a 2,2 0 1,1 4,0 2,2 0 1,1 -4,0',
+        fillColor: color,
+        fillOpacity: 1,
+        strokeColor: '#000',
+        strokeWeight: 2,
+        scale: 1,
+   };
+}
 
 
 
