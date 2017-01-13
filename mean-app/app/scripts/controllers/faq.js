cbd81888b4f1b444dac59cae40b53144327333ae
Alicia Marin <ma202240@etu.unice.fr>
2017-01-13 11:34:09 +0100

    TGJ-109 TGJ-117 ajoute le changement d'onglet entre riddle et photo

--- mean-app/app/scripts/controllers/faq.js
+++ mean-app/app/scripts/controllers/faq.js
@@ -3,10 +3,25 @@
 angular.module('eldoragoApp')
   .controller('FaqCtrl', function ($scope) {
 
+    $scope.initFaq = function() {
+      $scope.typeQuestSelected = 'riddle';
+    }
+
+    /** Boolean **/
+    $scope.isSelected = function(type) {
+      return $scope.typeQuestSelected === type;
+    };
+
+    /** Setter **/
     $scope.selectTheme = function(theme) {
       $scope.themeSelected = theme;
     }
 
+    $scope.selectTypeQuest = function(type) {
+      $scope.typeQuestSelected = type;
+    }
+
+    /** Remove **/
     $scope.removeTheme = function() {
       $scope.themeSelected = null;
     }
