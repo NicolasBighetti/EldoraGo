d071a9e521d7395b21e7dcdc1a26e41ab5ec2c0d
Nicolas Bighetti <nicobighetti@gmail.com>
2017-01-18 14:07:11 +0100

    Ajout de la connection websocket
    TGJ-136

--- client-mobile/www/js/app.js
+++ client-mobile/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services', 'angular-timeline', 'starter.historique', 'starter.cot-datas', 'starter.notifications'])
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services', 'angular-timeline', 'starter.historique', 'starter.cot-datas', 'starter.notifications', 'starter.go-socket'])
 
 
 .run(function($ionicPlatform) {
